345e669a7963d8cf6a1337162f93e4ec55c6b40f
Erfan.Bhr <erfanbahari6@gmail.com>
2026-06-03 00:09:29 +0330

    Stage 2 - TinyTimber startup working

--- example/greenhouse/src/main.c
+++ example/greenhouse/src/main.c
@@ -4,7 +4,12 @@
 
 void startup(void)
 {
-    printf("Greenhouse System Started\n");
+    printf("=================================\n");
+    printf(" Greenhouse Monitoring System\n");
+    printf(" TinyTimber Started Successfully\n");
+    printf("=================================\n");
+
+    printf("Startup Function Executed\n");
 }
 
 ENV_STARTUP(startup);
